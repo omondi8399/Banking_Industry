4c0837d23916f15e710e0797ad3bee2d31340b61
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-01-18 15:30:34 +0300

    Initial commit

--- contract.sol
+++ contract.sol
@@ -18,8 +18,36 @@ contract Banking {
     function withdraw(uint256 amount) public {
         require(msg.sender == owner, "Only the owner can withdraw funds");
         require(amount <= balances[msg.sender], "Insufficient funds");
-        require(condition);
-        
+        require(amount > 0, "Withdrawal amount must be greater than 0");
+        payable(msg.sender).transfer(amount);
+        balances[msg.sender] -= amount;        
+    }
+
+    function transfer(address payable recipient, uint256 amount) public {
+        require(amount <= balances[msg.sender], "Insufficient funds");
+        require(amount > 0, "Transfer amount must be greater than 0");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+    }
+
+    function getBalance(address payable user) public view returns (uint256) {
+        return balances[user];
+    }
+
+    function grantAccess (address payable user) public {
+        require(msg.sender == owner, "Only the owner can grant access.");
+        owner = user;
+    }
+
+    function revokeAccess(address payable user) public {
+        require(msg.sender == owner, "Only the owner can revoke access.");
+        require(user != owner, "Cannot revoke access for the current owner");
+        owner = payable(msg.sender);
+    }
+
+    function destroy() public {
+        require(msg.sender == owner, "Only the owner can destroy the contract");
+        selfdestruct(owner);
     }
 
 }
